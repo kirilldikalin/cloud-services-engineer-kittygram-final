e4202b8caaf593a24f31300c8c2d2d9cc58a98a4
kirilldikalin <dik.kir200010@gmail.com>
2025-09-19 14:02:46 +0300

    add_local_test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,14 +3,15 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
+SECRET_KEY = os.getenv('SECRET_KEY') or get_random_secret_key()
 
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('1','true','yes','on')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
