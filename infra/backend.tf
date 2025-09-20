88584a394901978d6fdb322a22f079c2e46fbc33
kirilldikalin <dik.kir200010@gmail.com>
2025-09-20 20:21:37 +0300

    terraform: fix backend init (endpoint), clean variables.tf; minimal backend block

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,13 +1,3 @@
 terraform {
-  backend "s3" {
-    # фиксированный endpoint для Object Storage
-    endpoints = { s3 = "https://storage.yandexcloud.net" }
-    region    = "ru-central1"
-    key       = "tf-state.tfstate"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-  }
+  backend "s3" {}
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,20 +1,54 @@
-variable "cloud_id"        { type = string }
-variable "folder_id"       { type = string }
-variable "tf_state_bucket" { type = string }
-
+variable "cloud_id" {
+  type = string
+}
+variable "folder_id" {
+  type = string
+}
 variable "sa_key_file" {
   type    = string
   default = ""
 }
+variable "tf_state_bucket" {
+  type = string
+}
 
-variable "network_name" { type = string  default = "kitty-vpc" }
-variable "subnet_cidr"  { type = string  default = "10.10.0.0/24" }
-variable "zone"         { type = string  default = "ru-central1-a" }
+variable "network_name" {
+  type    = string
+  default = "kitty-vpc"
+}
+variable "subnet_cidr" {
+  type    = string
+  default = "10.10.0.0/24"
+}
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
+}
 
-variable "vm_name"       { type = string  default = "kitty-vm" }
-variable "vm_ssh_user"   { type = string  default = "student" }
-variable "vm_ssh_pubkey" { type = string }
-variable "vm_platform"   { type = string  default = "standard-v3" }
-variable "vm_cores"      { type = number  default = 2 }
-variable "vm_mem_gb"     { type = number  default = 4 }
-variable "vm_disk_gb"    { type = number  default = 20 }
+variable "vm_name" {
+  type    = string
+  default = "kitty-vm"
+}
+variable "vm_ssh_user" {
+  type    = string
+  default = "student"
+}
+variable "vm_ssh_pubkey" {
+  type = string
+}
+variable "vm_platform" {
+  type    = string
+  default = "standard-v3"
+}
+variable "vm_cores" {
+  type    = number
+  default = 2
+}
+variable "vm_mem_gb" {
+  type    = number
+  default = 4
+}
+variable "vm_disk_gb" {
+  type    = number
+  default = 20
+}
